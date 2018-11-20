68974ee230bee3a2d7319ffdda6c6f47777b8f8a
sannaluo <smo.luostarinen@gmail.com>
2018-11-20 12:03:33 +0200

    haku databasesta ja värit

--- src/App.js
+++ src/App.js
@@ -6,38 +6,6 @@ import Select from "react-select";
 import firebase from './firebase.js';
 import * as sanna from './sanna.js';
 
-
-/*
-let skills;
-
-// Listassa näkyvä yksittäinen osaaminen
-// Parametrinä annetaan objekti jonka sisällä on osaamisen nimi, kategoria ja väri
-function Skill(props) {
-    return (
-        <li className="Skill">
-            <div className="skillColorTag" style={{backgroundColor : props.skillInfo.color}}>
-            </div>
-            <div className="skillContent">
-                <p>{props.skillInfo.category}</p>
-                <h3>{props.skillInfo.name}</h3>
-            </div>
-        </li>
-    );
-}
-
-// Osaamisia sisältävä lista.
-// Parametrinä annetaan lista kaikista osaamisista.
-function SkillList(props) {
-    return (
-        <ul className="SkillList">
-            {/* Looppaa kaikki parametrina annetun listan alkiot ja tekee jokaisesta osaamisen(Skill) */ /*}
-            {props.skillList.map( (skill, i)=> {
-                return <Skill key={i} skillInfo={skill}/>
-            } )}
-        </ul>
-    );
-}
-*/
 // Lähetetään osaaminen
 const sendToDb = () => {
     // TODO haetaan arvot lomakkeesta
@@ -65,12 +33,11 @@ const sendToDb = () => {
     sanna.testDb();
 };
 
-
 // Nappi lähettää databaseen arvon
 function DbButton() {
     return (
         <a href="#" onClick={sendToDb}>
-            Click me
+            Testinappi
         </a>
     );
 }
@@ -79,12 +46,11 @@ function DbButton() {
 function Header() {
     return (
         <div className="Header">
-            <h1>Yläpalkki</h1>
+            <h1>OON</h1>
             <DbButton/>
         </div>
     );
 }
-
 
 //Uusi osaaminen nappula ja modaalin sisältöä
 function New() {
@@ -111,83 +77,121 @@ function New() {
             <h1> + Lisää uusi osaaminen </h1>
         </ModalButton>
     );
-
 }
 
+//TODO onko tää hyvä tehdä täällä frontissa?
+/**
+ * Valitaan väri kategorian mukaan.
+ * @param category
+ * @returns {string}
+ */
+function getColour(category) {
+    let colour = '#0000';
+    switch (category) {
+        case 'autot':
+            colour = '#590375';
+            break;
+        case 'it':
+            colour = '#89023E';
+            break;
+        case 'musiikki':
+            colour = '#e48255';
+            break;
+        case 'ruuanlaitto':
+            colour = '#136F63';
+            break;
+        case 'talotyöt':
+            colour = '#395C89';
+            break;
+        case 'nikkarointi':
+            colour = '#FFBA5C';
+            break;
+    }
+    return colour;
+}
 
+/**
+ * Yksittäinen osaaminen listassa.
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+function Skill(props) {
+    let colour = getColour(props.skillInfo.category);
+    console.log(colour);
+
+    return (
+        <li className="Skill">
+            <div className="skillColorTag" style={{backgroundColor : colour}}>
+            </div>
+            <div className="skillContent">
+                <p>{props.skillInfo.category}</p>
+                <h3>{props.skillInfo.title}</h3>
+                <p>{props.skillInfo.tools}</p>
+                <p>{props.skillInfo.steps}</p>
+                <p>{props.skillInfo.picture}</p>
+                <p>{props.skillInfo.newsection1}</p>
+            </div>
+        </li>
+    );
+}
+
+/**
+ * Osaamisia sisältävä lista.
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+function SkillList(props) {
+    let postArray = props.posts;
+
+    // Jos objekti on tyhjä, annetaan sille arvo. Näin käy kun tietokannasta ei ole haettu riittävän nopeasti.
+    if(Object.keys(postArray).length === 0 && postArray.constructor === Object){
+        postArray = [''];
+    }
+
+    return (
+        <ul className="SkillList">
+                {/* Looppaa kaikki parametrina annetun listan alkiot ja tekee jokaisesta osaamisen(Skill) */}
+                {postArray.map((r, post) => {
+                    return <Skill key={post} skillInfo={r}/>
+                })}
+        </ul>
+    );
+}
+
+/**
+ * Osaamiset eli postit listattuna
+ */
 class Posts extends Component {
     constructor(props){
         super(props);
         this.state = {
-
             posts: {}
         };
     }
+
     componentDidMount(){
         const postsRef = firebase.database().ref().child('posts/userid/');
-
 
         postsRef.once('value', snap => {
                 this.setState({
                   posts: snap.val()
                 });
-                console.log(snap.val());
+              //  console.log(snap.val());
             });
-
-     /*   const listOfPosts = this.state.listOfPosts.map(post =>
-            <div>
-                <h1>{post.id}</h1>
-                <h1>{post.category}</h1>
-                <h1>{post.title}</h1>
-                <h1>{post.tools}</h1>
-                <h1>{post.steps}</h1>
-                <h1>{post.picture}</h1>
-                <h1>{post.newsection1}</h1>
-            </div>
-        );
-        console.log(listOfPosts);
-*/
-
-
     }
+
     render() {
-        console.log(this.state.posts);
-
-        let postArray = this.state.posts;
-        let renderedList = [];
-
-        for(let post in postArray) {
-            if (postArray.hasOwnProperty(post)) {
-                // console.log(post); // key
-                console.log(postArray[post]); // value
-
-                let r = postArray[post];
-
-                renderedList.push(<li key={post}> <ul>
-                    <li>{r.category} </li>
-                    <li>{r.title}</li>
-                    <li>{r.tools}</li>
-                    <li>{r.steps}</li>
-                    <li>{r.picture}</li>
-                    <li>{r.newsection1}</li>
-                </ul></li>);
-
-                // rendered += '<li>' + postArray[post].category + '</li>';
-            }
-        }
+       // console.log(this.state.posts);
 
         return (
             <div>
-                <ul>
-                    {
-                        renderedList
-                    }
-                </ul>
+                <SkillList posts={this.state.posts}/>
             </div>
         );
     }
 }
-
 
 // Sivun varsinanen sisältö
 function Main() {
@@ -209,162 +213,3 @@ function App() {
 }
 
 export default App;
-
-/*
-// Sivun varsinanen sisältö
-function Main() {
-    // Placeholder lista tietokannasta haetuista osaamisista.
-    const skillsList = [
-        {   name: "renkaat",
-            category: "autot",
-            color: "#a826d9"},
-
-        {   name: "makkaraperunat",
-            category: "ruoka",
-            color: "#52bcd9"},
-
-        {   name: "kurkkumopo",
-            category: "autot",
-            color: "#a826d9"},
-
-        {   name: "1 burtsa + kylmä kolle",
-            category: "ruoka",
-            color: "#52bcd9"}
-    ];
-
-   // skills = sanna.getAllPosts();
-   // console.log(skills);
-/*
-    return (
-        <div className="Main">
-            <SkillList skillList={skillsList}/>
-        </div>
-    );
-*/
-/*
-    const listOfPosts = this.state.listOfPosts.map(post =>
-        <div>
-            <h1>{post.id}</h1>
-            <h1>{post.category}</h1>
-            <h1>{post.title}</h1>
-            <h1>{post.tools}</h1>
-            <h1>{post.steps}</h1>
-            <h1>{post.picture}</h1>
-            <h1>{post.newsection1}</h1>
-        </div>
-    );
-    return (
-        <div>{listOfPosts}</div>
-    );
-}
-*/
-/*
-function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <New/>
-            <Main/>
-        </div>
-    );
-}
-
-export default App;
-*/
-
-/*
-
-class App extends Component {
-
-    constructor(){
-        super();
-        this.state = {
-            listOfPosts: []
-        };
-    }
-
-
-    componentDidMount() {
-        const rootRef = firebase.database().ref().child('posts/userid');
-
-        rootRef.once('value').then(function (snap) {
-            const previousList = this.state.listOfPosts;
-
-            console.log(snap);
-
-            previousList.append({
-                id: snap.key,
-                category: snap.val().category,
-                title: snap.val().title,
-                rating: snap.val().rating,
-                tools: snap.val().tools,
-                steps: snap.val().steps,
-                picture: snap.val().picture,
-                newsection1: snap.val().newsection1
-            });
-            this.setState({
-                listOfPosts: previousList
-            });
-        });
-
-        console.log(this.state.listOfPosts);
-
-        rootRef.on('child_added', snap => {
-            const previousList = this.state.listOfPosts;
-            previousList.append({
-                id: snap.key,
-                category: snap.val().category,
-                title: snap.val().title,
-                rating: snap.val().rating,
-                tools: snap.val().tools,
-                steps: snap.val().steps,
-                picture: snap.val().picture,
-                newsection1: snap.val().newsection1
-            });
-            this.setState({
-                listOfPosts: previousList
-            });
-        });
-
-
-        // tää pitäis saada näkymään
-        let listOfPosts = this.state.listOfPosts.map(post =>
-            <div>
-                <h1>{post.id}</h1>
-                <h1>{post.category}</h1>
-                <h1>{post.title}</h1>
-                <h1>{post.tools}</h1>
-                <h1>{post.steps}</h1>
-                <h1>{post.picture}</h1>
-                <h1>{post.newsection1}</h1>
-            </div>
-        );
-        /*   return (
-               <div>{listOfPosts}</div>
-           );
-
-   */
-/*
-    }
-    render() {
-        return (
-            /*
-            <div className="App">
-                <h1>{this.state.id}</h1>
-                <h1>{this.state.latitude}</h1>
-                <h1>{this.state.longitude}</h1>
-            </div>
-            */
-/*
-            <div className="App">
-                <Header/>
-                <New/>
-                <div>{this.state.listOfPosts}</div>
-            </div>
-        );
-    }
-}
-
-*/
-
-// export default App;
