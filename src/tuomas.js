55a1b8f62ff63ebc8b65cb5d76aca9a6877ef210
tuomaju <tuomas.juopperi@metropolia.fi>
2018-12-08 20:04:12 +0200

    jepjoo

--- src/tuomas.js
+++ src/tuomas.js
@@ -1,27 +1,50 @@
 import firebase from "./firebase/firebase";
 import JsPDF from "jspdf";
 
-export const makePDF = () => {
-    let doc = new JsPDF();
+
+
+
+export const makePDF = (props) => {
+    let doc = new JsPDF("p", "px", "a4", true);
 
     let bodyElement = document.querySelector('.sharedPaper');
+    let name = props.fName + '_' + props.lName + '_osaamiset.pdf';
 
+    let specialElementHandlers = {
+        '#ignorePDF1': function(element, renderer){
+            return true;
+        },
+        '.skillPicture': function(element, renderer){
+            return true;
+        }
+    };
 
     doc.fromHTML(
         bodyElement,
-        15,
-        15,
+       25,
+        25,
         {
-            'width': 180,'elementHandlers': {'#ignorePDF1': function (element, renderer) {
-                    return true;
-                }}
-        });
+            'width': 298,
+            'elementHandlers': specialElementHandlers
+        },
+        () => {
 
-    doc.save('test.pdf');
+            doc.save(name);
+        }, 0);
+
 
 };
 
-export function copyToClipboard(str){
+export const makeBetterPDF = (props) => {
+    let doc = new JsPDF("p", "mm", "a4", true);
+
+    let name = props.fName + '_' + props.lName + '_osaamiset.pdf';
+
+
+};
+
+
+export function copyToClipboard(str) {
     const el = document.createElement('textarea');
     el.value = str;
     document.body.appendChild(el);
@@ -43,7 +66,7 @@ export function profileToDb(values) {
 function randomString(length) {
     const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
 
-    if (! length) {
+    if (!length) {
         length = Math.floor(Math.random() * chars.length);
     }
 
@@ -54,7 +77,7 @@ function randomString(length) {
     return str;
 }
 
-export function randomizeUrl () {
+export function randomizeUrl() {
     let randomStr = randomString(8);
 
     console.log(randomStr);
@@ -66,7 +89,7 @@ export function randomizeUrl () {
     profileToDb(values);
 }
 
-export function sharePosts (listOfSkills) {
+export function sharePosts(listOfSkills) {
     firebase.database().ref('shareToEveryone/userid/').set({
         posts: listOfSkills,
     });
